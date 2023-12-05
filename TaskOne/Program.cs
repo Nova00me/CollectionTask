dc920638577a41eeaf06c095a265f882025aad90
Nova <Nova@DESKTOP-L1FJN9G>
2023-12-05 22:01:44 +0600

    Добавьте файлы проекта.

---
+++ TaskOne/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace TaskOne
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> bookList = new List<string>();
+            Stopwatch sw = new Stopwatch();
+
+            var book = File.ReadAllText(@"C:\Users\Nova\Desktop\Text1.txt"); //путь 
+
+            bookList.AddRange(book.Split(" ")); 
+            
+            LinkedList<string> bookLinkedList = new LinkedList<string>(bookList);
+
+            #region ListSet
+            sw.Start();
+
+            bookList.Insert(33, "Hello");
+
+            sw.Stop();
+
+            Console.WriteLine($"Скорость вставки слова Hello в индекс 33 = {sw.Elapsed.TotalMilliseconds} милесекунд");
+            #endregion
+            #region LinkedListSet
+            int temsp = 0;
+            foreach (var item in bookLinkedList)
+            {
+                temsp++;
+                if(temsp == 33)
+                {
+                    var ssa = bookLinkedList.Find(item);
+                    sw.Start();
+                    bookLinkedList.AddAfter(ssa, "Hello");
+                    sw.Stop();
+                    Console.WriteLine($"Скорость вставки слова Hello в индекс 33 = {sw.Elapsed.TotalMilliseconds} миллесекунд");
+                    break;
+                }
+            }
+            #endregion
+
+
+        }
+    }
+}
---
+++ TaskTwo/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TaskTwo
+{
+    class Pogram
+    {
+        public static void Main(string[] args)
+        {
+            string Text = File.ReadAllText(@"C:\Users\Nova\Desktop\Text1.txt");
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            SortedSet<int> ints = new SortedSet<int>();
+            char[] chars = { ' ', ',', '.', '(', ')', '<', '>', '\"', '\'', ':', ';', '-' };
+
+
+            string[] worlds = Text.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in worlds)
+            {
+                if(counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts[word] = 1;
+                }
+            }
+            var sortedWords = from pair in counts orderby pair.Value descending select pair; //эту строку взял из chat - gpt
+
+            foreach (var pair in sortedWords.Take(10))
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+
+    }
+}
